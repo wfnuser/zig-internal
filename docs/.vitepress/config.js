b416bf1ee6631286f09baa3252620a4a68ffb654
wfnuser <wfnuser@hotmail.com>
2023-12-10 00:25:42 +0800

    init vitepress

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: "Zig",
+  base: "/zig-internal/",
+  themeConfig: {
+    sidebar: {
+      "/": [
+        {
+          text: "Zig语言实现",
+          link: "/",
+        },
+        {
+          text: "1. 快速上手",
+          items: [{ text: "1.1 Hello, World!", link: "/entry/hello-world" }],
+        },
+      ],
+    },
+  },
+};
